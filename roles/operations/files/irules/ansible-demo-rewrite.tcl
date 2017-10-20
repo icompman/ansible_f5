78d933b658f48aa3a016571ab0d7bcb47a396e16
mcheo <michael.cheo@gmail.com>
2017-10-20 13:53:00 +0800

    Update ansible-demo-rewrite.tcl

--- roles/operations/files/irules/ansible-demo-rewrite.tcl
+++ roles/operations/files/irules/ansible-demo-rewrite.tcl
@@ -1,19 +1,27 @@
 when HTTP_REQUEST {
 
-	if { [HTTP::uri] ends_with "/" } {
+	if { [HTTP::uri] ends_with ".html" } {
 	
 	    if { ! [HTTP::cookie exists "visited_maintenance_page"] || [HTTP::cookie value "visited_maintenance_page"] != "true" } {
         
 		 	HTTP::respond 200 content {
 				<html>
-	      			<head><meta http-equiv="set-cookie" content="visited_maintenance_page=true; expires=10">
+	      			<head>
+	      				<!--<meta http-equiv="set-cookie" content="visited_maintenance_page=true; expires=10">-->
 	      				<title>Maintenance Page</title>
 	      			</head>
+	      			<script>
+	      				   	var date = new Date();
+   							date.setTime(date.getTime()+(30*1000));
+   							var expires = "; expires="+date.toGMTString();
+   							document.cookie = "visited_maintenance_page=true " + expires;
+	      			</script>
 	      			<body>
-	      			     <center><img src="https://www.dropbox.com/s/1shixntablezykb/ansible_f5demo.png?raw=1"</img><</center>
+	      			     <!--<center><img src="https://www.dropbox.com/s/1shixntablezykb/ansible_f5demo_test.png?raw=1"</img><</center>-->
+	      			     <center><img src="http://localhost:8080/ansible_f5demo.png"</img><</center>
 	      			     <br/>
 	      			     <h1><center>This site is scheduled for maintenance in the next hour. <br/>
-	      			     	Sorry for any inconvenience caused. Please click to <a href="/">continue</a></center></h1>
+	      			     	Sorry for any inconvenience caused. Please click to <a href="/pageindex_onpremise.html">continue</a></center></h1>
 	      			</body>
 	      		</html>
 		 	}
