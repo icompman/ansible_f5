87b3e8dc7bfc22e9217eef0c874de15b55e55142
Narongrit Intasunta <unknownvyruz@gmail.com>
2017-12-14 08:42:55 +0700

    Update post_slack.py

--- post_slack.py
+++ post_slack.py
@@ -1,7 +1,7 @@
 from slacker import Slacker
 import sys
 
-slack = Slacker('{slack_token}')
+slack = Slacker('xoxb-283252429493-Oj9UwziwOGBm6kCtJFj5oGQN')
 
 # Send a message to #general channel
 slack.chat.post_message('#demo', sys.argv[1])
